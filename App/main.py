31273f9909e1b0b37f1d6b4811d3fe06ce26d636
Ricky <rickydiaz1802@gmail.com>
2025-05-11 20:51:55 -0500

    2

---
+++ App/main.py
@@ -0,0 +1,30 @@
+from dao.database import Conexion
+from routers import actividadesRouter, emocionesRouter, entradasRouter, usuariosRouter
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+app.include_router(pedidosRouter.router)
+app.include_router(productosRouter.router)
+app.include_router(usuariosRouter.router)
+@app.get("/")
+async def home():
+    salida = {"Bienvenido de vuelta señor Stark"}
+    return salida
+
+@app.on_event("startup")
+async def startup():
+    print("Conectando con MongoDB")
+    conexion = Conexion()
+    app.conexion=conexion
+    app.db=conexion.getDB()
+
+@app.on_event("shutdown")
+async def shutdown():
+    print("Cerrando la conexion con MongoDB")
+    app.conexion.cerrar()
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", reload=True)
---
+++ App/routers/usuariosRouter.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/usuarios", tags=["Usuarios"])
